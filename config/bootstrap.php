84267004f9850e4f02acf6b210ed3518dab45ad2
Aestus <currorecre@gmail.com>
2021-01-01 23:47:28 +0100

    modifico bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,6 +9,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     foreach ($env as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
+        
     }
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
